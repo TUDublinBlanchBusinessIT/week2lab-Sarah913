a4f81782c5b7ee4a507ee0358926645060bf34cf
Sarah Forde <you@example.com>
2025-02-10 17:44:00 +0000

    Completed Part A

---
+++ PartA.py
@@ -0,0 +1,20 @@
+#Sarah Forde
+#10/02/2025
+#Week 3 Lab 1
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+    
+    
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
+
