3958c3b9e4cd47ef1f4aa1ef26a1b315703df1ef
Sarah Forde <you@example.com>
2025-02-10 17:54:12 +0000

    Completed Part B

---
+++ PartB.py
@@ -0,0 +1,17 @@
+#Sarah Forde
+#10/02/2025
+#Week 3 Lab 1
+
+def printSteps(a,b):
+    for i in range (a,b):
+        print("Step " + str(i))
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
+
